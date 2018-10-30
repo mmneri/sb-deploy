48bfc607fd152f3855da49ad829db6294ea3697c
Maramoura\Maram <mneri.marouen@gmail.com>
2018-10-30 08:07:33 +0100

    correctif

--- utilities.groovy
+++ utilities.groovy
@@ -4,7 +4,7 @@
 //
 // ##################################################################################
 
-def get_branch_type(String branch_name) {
+public get_branch_type(String branch_name) {
     //Must be specified according to <flowInitContext> configuration of jgitflow-maven-plugin in pom.xml
     def dev_pattern = ".*dev"
     def release_pattern = ".*release/.*"
@@ -26,7 +26,7 @@ def get_branch_type(String branch_name) {
     }
 }
 
-def get_branch_deployment_environment(String branch_type) {
+public get_branch_deployment_environment(String branch_type) {
     if (branch_type == "dev") {
         return "dev"
     } else if (branch_type == "release") {
@@ -38,7 +38,7 @@ def get_branch_deployment_environment(String branch_type) {
     }
 }
 
-def mvn(String goals) {
+public mvn(String goals) {
     def mvnHome = tool "mvn"
 
     if (isUnix()) {
@@ -55,7 +55,7 @@ def mvn(String goals) {
 //
 // ##################################################################################
 
-def compareVersions ( requiredVersions, currentVersions) {
+public compareVersions ( requiredVersions, currentVersions) {
 
     currentapps = currentVersions.stringPropertyNames().toArray()
     reqapps = requiredVersions.stringPropertyNames().toArray()
@@ -77,33 +77,33 @@ def compareVersions ( requiredVersions, currentVersions) {
 }
 
 
-def decom(app, revision) {
+public decom(app, revision) {
     node ("$app-deploy-runner") {
         log ("Decomission", """Perform the decomission steps here for app: $app eg call sh /scripts/$app/decom nft""")
         sleep time: sleepDuration
     }
 }
 
-def deploy(app, revision) {
+public deploy(app, revision) {
     node ("$app-deploy-runner") {
         log ("Deploy", """Perform the deploy steps here for app: $app:$revision eg call sh /scripts/$app/deploy nft $revision""")
         sleep time: sleepDuration
     }
 }
 
-def performNFT() {
+public performNFT() {
     node ("nft-runner") {
         log ("Run NFT",  "Perform the NFT steps")
         sleep time: sleepDuration
     }
 }
 
-def triggerRun() {
+public triggerRun() {
     log "Trigger build", "Triggering a new build"
     build job: env.JOB_NAME, propagate: false, wait: false
 }
 
-def getBlockedBuilds(fullName) {
+public getBlockedBuilds(fullName) {
     def q = Jenkins.instance.queue
     items = q.items
     Items[] matches = []
@@ -116,18 +116,18 @@ def getBlockedBuilds(fullName) {
     return matches
 }
 
-def writePropertiesFile(props, file) {
+public writePropertiesFile(props, file) {
     log "WriteProperties", "File = $file"
     writeFile file: file, text: writeProperties(props)
 }
 
-@NonCPS def writeProperties (props) {
+@NonCPS public writeProperties (props) {
     def sw = new StringWriter()
     props.store(sw, null)
     return sw.toString()
 }
 
-def readPropertiesFromFile (file) {
+public readPropertiesFromFile (file) {
     log "ReadProperties", "File = $file"
     def str = readFile file: file, charset : 'utf-8'
     def sr = new StringReader(str)
@@ -136,10 +136,12 @@ def readPropertiesFromFile (file) {
     return props
 }
 
-def log (step, msg) {
+public log (step, msg) {
 
     echo """************************************************************
 			Step: $step
 			$msg
 			************************************************************"""
 }
+
+return this;
