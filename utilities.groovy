09861d322e360f960a3da6831ba09e0df8fcda64
Maramoura\Maram <mneri.marouen@gmail.com>
2018-12-08 01:24:22 +0100

    update

--- utilities.groovy
+++ utilities.groovy
@@ -142,11 +142,13 @@ public deploy(app, revision) {
                 		def appCont =  appContext[1]         		
 	                	cmd "curl \"http://$USERNAME:$PASSWORD@localhost:8181/manager/text/stop?path=/fff&version=$appCont\""
 				    	cmd "curl \"http://$USERNAME:$PASSWORD@localhost:8181/manager/text/undeploy?path=/fff&version=$appCont"
+				    	cmd "curl --upload-file deploy/$app/$revision/target/$appVersion http://$USERNAME:$PASSWORD@localhost:8181/manager/text/deploy?path=/fff&version=$appCont&update=true"
                 	}else{
 	                	cmd "curl http://$USERNAME:$PASSWORD@localhost:8181/manager/text/stop?path=/fff"
 				    	cmd "curl http://$USERNAME:$PASSWORD@localhost:8181/manager/text/undeploy?path=/fff"
+				    	cmd "curl --upload-file deploy/$app/$revision/target/$appVersion http://$USERNAME:$PASSWORD@localhost:8181/manager/text/deploy?path=/fff"
                 	}
-				    cmd "curl --upload-file deploy/$app/$revision/target/$appVersion http://$USERNAME:$PASSWORD@localhost:8181/manager/text/deploy?path=/fff"
+				    
                 }else{
                 	cmd "curl --upload-file deploy/$app/$revision/target/$appVersion http://$USERNAME:$PASSWORD@localhost:8181/manager/text/deploy?path=/fff"
                 }
